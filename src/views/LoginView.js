aae0fb699102fa9b335bc2131bb6a0478f6b7fec
olo2552 <a.wielgorski@protonmail.com>
2018-05-24 22:10:07 +0200

    Login view 1st iteration: no styling

---
+++ src/views/LoginView.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import SteemConnectButton from '../components/LoginWithSC2';
+
+class LoginView extends React.Component {
+    render() {
+        return (
+            <div>
+                Welcome to Utopian internal helper tool!
+                <SteemConnectButton /> 
+            </div>
+        );
+    };
+} 
+
+export default LoginView;
