a5609742efb25a78ee0c1d62a5009119a7a608e2
olo2552 <a.wielgorski@protonmail.com>
2018-05-24 22:04:09 +0200

    first iteration boilerplate

---
+++ src/index.js
@@ -0,0 +1,55 @@
+// React dependencies
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Redux & dependencies
+import { 
+    createStore,
+    combineReducers,
+    applyMiddleware,
+    compose,
+} from 'redux';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import loggerMiddleware from 'redux-logger';
+import sagaMiddlewareFactory from 'redux-saga';
+
+// Redux flow
+import * as reducers from './reducers';
+import rootSaga from './sagas';
+import Routes from './routes';
+
+import registerServiceWorker from './registerServiceWorker';
+
+import 'normalize.css';
+
+// dotenv support
+require('dotenv').config();
+const sagaMiddleware = sagaMiddlewareFactory();
+
+// creating Redux Store & applying uppermentioned middleware
+const store = createStore(
+    combineReducers({
+        ...reducers,
+    }),
+    compose(
+        applyMiddleware(
+            sagaMiddleware,
+            loggerMiddleware,
+        ),
+        window.devToolsExtension ? window.devToolsExtension() : f => f,
+    ),
+);
+
+sagaMiddleware.run(rootSaga);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Router>
+            <Switch>
+                <Routes />
+            </Switch>
+        </Router>
+    </Provider>,
+    document.getElementById('root'));
+registerServiceWorker();
