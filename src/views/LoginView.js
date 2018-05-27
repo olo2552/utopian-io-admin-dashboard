880872e26666b599efad1ddedb2eb5d2464bd32e
olo2552 <a.wielgorski@protonmail.com>
2018-05-27 12:46:20 +0200

    Prettier support

--- src/views/LoginView.js
+++ src/views/LoginView.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import React from "react";
 
-import SteemConnectButton from '../components/LoginWithSC2';
+import SteemConnectButton from "../components/LoginWithSC2";
 
 class LoginView extends React.Component {
-    render() {
-        return (
-            <div>
-                Welcome to Utopian internal helper tool!
-                <SteemConnectButton /> 
-            </div>
-        );
-    };
-} 
+  render() {
+    return (
+      <div>
+        Welcome to Utopian internal helper tool!
+        <SteemConnectButton />
+      </div>
+    );
+  }
+}
 
 export default LoginView;
