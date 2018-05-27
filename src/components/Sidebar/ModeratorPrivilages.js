7ccdbb2446cf245049e5ebf441dfd996ecf0febe
olo2552 <a.wielgorski@protonmail.com>
2018-05-27 12:44:26 +0200

    Sidebar: Moderator Privilages

---
+++ src/components/Sidebar/ModeratorPrivilages.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default () => ([
+    <li className="sidebar-header" key="sidebar-header">Moderator's Privilages</li>,
+    <li className="nav-item" key="nav-item">
+        <a className="nav-link" href="#">Review Conributions</a>
+        <a className="nav-link" href="#">Browse Contributions</a>
+    </li>
+]);
