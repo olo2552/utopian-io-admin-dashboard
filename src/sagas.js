96b875d727d95f8ba0261be42d01c2af1c3bdcb9
olo2552 <a.wielgorski@protonmail.com>
2018-05-27 12:38:07 +0200

    SC2 user credentials fetching

--- src/sagas.js
+++ src/sagas.js
@@ -1,11 +1,34 @@
-import { all } from 'redux-saga/effects'
+import { all, call, takeLatest, put } from "redux-saga/effects";
 
-function* helloSaga() {
-    console.log('Hello Sagas!')
+import axios from "axios";
+import SC2SDK from "./utils/sc2sdk";
+function* fetchSC2UserData({ access_token }) {
+  //  TODO: Data caching
+  try {
+    const response = yield call(
+      axios.post,
+      "https://steemconnect.com/api/me",
+      {},
+      {
+        headers: {
+          Authorization: access_token
+        }
+      }
+    );
+
+    yield put({
+      type: "SAVE_SC2_USER_DATA",
+      data: response.data
+    });
+  } catch (e) {
+    console.log(e);
+  }
 }
-  
+
+function* watchFetchSC2UserData() {
+  yield takeLatest("FETCH_SC2_USER_DATA", fetchSC2UserData);
+}
+
 export default function* rootSaga() {
-    yield all([
-      helloSaga(),
-    ]);
-};
+  yield all([watchFetchSC2UserData()]);
+}
