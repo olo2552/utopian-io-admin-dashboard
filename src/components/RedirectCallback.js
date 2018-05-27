e2db798ac7dd9d3e64e2ce4a075e46ea98600dc2
olo2552 <a.wielgorski@protonmail.com>
2018-05-27 12:39:05 +0200

    SC2 redirect component

---
+++ src/components/RedirectCallback.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+import { withRouter } from "react-router-dom";
+import SC2SDK from "../utils/sc2sdk";
+import qs from "query-string";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+
+import { fetchSC2UserData } from "../actionCreators";
+
+class RedirectCallback extends React.Component {
+  static propTypes = {
+    match: PropTypes.object.isRequired,
+    location: PropTypes.object.isRequired,
+    history: PropTypes.object.isRequired
+  };
+
+  componentDidMount = () => {
+    const parsed = qs.parse(this.props.location.search);
+    SC2SDK.setAccessToken(parsed.access_token);
+    this.props.fetchSC2UserData(parsed.access_token);
+    console.log(SC2SDK);
+  };
+
+  render() {
+    return null;
+  }
+}
+
+export default connect(null, { fetchSC2UserData })(
+  withRouter(RedirectCallback)
+);
