dde43a054d8d85945413627b98044e381bcb2d7a
olo2552 <a.wielgorski@protonmail.com>
2018-05-27 12:39:44 +0200

    Sidebar: Manager Components

---
+++ src/components/Sidebar/ManagerPrivilages.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default () => ([
+    <li className="sidebar-header" key="sidebar-header">Manager's Privilages</li>,
+    <li className="nav-item" key="nav-item">
+        <a className="nav-link" href="#">Browse Users</a>
+        <a className="nav-link" href="#">Ban User</a>
+    </li>
+]);
