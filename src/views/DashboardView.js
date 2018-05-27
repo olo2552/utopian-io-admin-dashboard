cc4072b91bf5802c8e89291335049d610b54d803
olo2552 <a.wielgorski@protonmail.com>
2018-05-27 12:45:57 +0200

    DashboardView Sidebar prototype

--- src/views/DashboardView.js
+++ src/views/DashboardView.js
@@ -1,3 +1,18 @@
-import React from 'react';
+import React from "react";
 
-export default () => <div>Dash</div>;
+import SidebarToggler from "../components/SidebarToggler";
+import Sidebar from "../components/Sidebar/Sidebar";
+import UserThumbnail from "../components/UserThumbnail";
+
+export default () => [
+  <header className="app-header navbar">
+    <SidebarToggler />
+    <UserThumbnail />
+  </header>,
+  <div className="app-body h-100">
+    <Sidebar />
+
+    <main className="main h-100">sdfsdf</main>
+  </div>,
+  <footer className="app-footer">footer</footer>
+];
